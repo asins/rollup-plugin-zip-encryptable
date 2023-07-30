3b742db00021b288c65d7ba324729b82ae5ed2ea
asins <asinsimple@gmail.com>
2023-07-30 23:15:39 +0800

    feat: mutil input file, output one zip

--- src/index.ts
+++ src/index.ts
@@ -44,11 +44,6 @@ interface IPluginOptions {
    */
   zlib?: object;
   /**
-   * Create zip file once. Useful in watch mode.
-   * @default false
-   */
-  createOnce?: boolean;
-  /**
    * Filter out the files that are not packaged into the zip package.
    * Returning true has just been filtered out.
    * @default null
@@ -56,36 +51,22 @@ interface IPluginOptions {
   filterFile?: (entry: OutputAsset | OutputChunk) => boolean;
 }
 
-const isAsset = (entry: OutputAsset | OutputChunk): entry is OutputAsset => (
-  entry.type === 'asset'
-)
-let isRegistered = false;
-
-
-const enum Cache {
-  distdir = 'distdir',
-  outfile = 'outfile',
-  sourcemapFile = 'sourcemapFile',
-}
-
 export default (options: IPluginOptions = {}): Plugin => {
   let zipCreated = false;
-  const {
-    createOnce = false,
-  } = options;
+  let outFilePath;
+  const fileList = [];
 
   return {
     name: 'archiverZip',
 
     generateBundle({ dir }): void {
-      if (createOnce && zipCreated) return;
+      if (zipCreated || outFilePath) return;
 
       // Save the output directory path
       let distDir = process.cwd();
       if (dir) {
         distDir = path.resolve(distDir, dir);
       }
-      this.cache.set(Cache.distdir, distDir);
       // Get options
       let outFile = options && options.file;
       const outDir = options && options.dir;
@@ -111,71 +92,58 @@ export default (options: IPluginOptions = {}): Plugin => {
         outFile = path.resolve(outDir || distDir, `${outFile}.zip`);
       }
       // Save the output file path
-      this.cache.set(Cache.outfile, outFile);
+      outFilePath = outFile;
     },
 
     writeBundle(_, bundle): Promise<void> {
-      if (createOnce && zipCreated) return Promise.resolve();
+      if (zipCreated) return Promise.resolve();
 
-      return new Promise(resolve$1 => {
-        // const distDir = this.cache.get(Cache.distdir);
-        let opt = {
-          password: options.password,
-          comment: options.comment,
-          forceLocalTime: options.forceLocalTime || true,
-          forceZip64: options.forceZip64,
-          namePrependSlash: options.namePrependSlash,
-          store: options.store,
-          zlib: options.zlib || { level: 9 },
-        } as IPluginOptions;
-        // 过虑掉不存在的参数
-        opt = Object.keys(opt).reduce((res: IPluginOptions, key: string) => {
-          if (opt[key] != null) res[key] = opt[key];
-          return res;
-        }, {} as IPluginOptions);
+      const dir = path.dirname(outFilePath);
 
-        let archive;
-        if (opt.password) {
-          if (!isRegistered) {
-            archiver.registerFormat('zip-encryptable', ZipEncryptable);
-            isRegistered = true;
-          }
-          /** @ts-ignore */
-          archive = archiver('zip-encryptable', opt);
-        } else {
-          archive = archiver('zip', opt);
-        }
+      Object.entries(bundle).forEach(([, entry]) => {
+        const { fileName } = entry;
+        if (typeof options.filterFile === 'function' && options.filterFile(entry)) return;
 
-        const outFile = this.cache.get(Cache.outfile);
-        const dir = path.dirname(outFile);
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir, { recursive: true });
-        }
-        const writeStream = fs.createWriteStream(outFile);
-        archive.pipe(writeStream);
-
-        Object.entries(bundle).forEach(([, entry]) => {
-          const { fileName } = entry;
-          if (typeof options.filterFile === 'function' && options.filterFile(entry)) return;
-
-          if (isAsset(entry)) {
-            const { source } = entry;
-            archive.append(Buffer.from(source), { name: fileName });
-          } else {
-            const { map, code } = entry;
-            archive.append(Buffer.from(code), { name: fileName });
-            if (map) {
-              const mapFile = `${fileName}.map`;
-              archive.append(Buffer.from(map.toString()), { name: mapFile });
-            }
-          }
-        });
-
-        archive.finalize();
-        zipCreated = true;
-        console.log(green('created zip file:') + ' ' + green(`${bold(outFile)}`));
-        writeStream.on('close', resolve$1);
+        const fileFullPath = path.join(dir, fileName);
+        fileList.push({ path: fileFullPath, fileName });
       });
+
+      return Promise.resolve();
     },
+    closeBundle() {
+      let opt = {
+        password: options.password,
+        comment: options.comment,
+        forceLocalTime: options.forceLocalTime || true,
+        forceZip64: options.forceZip64,
+        namePrependSlash: options.namePrependSlash,
+        store: options.store,
+        zlib: options.zlib || { level: 9 },
+      } as IPluginOptions;
+      // 过虑掉不存在的参数
+      opt = Object.keys(opt).reduce((res: IPluginOptions, key: string) => {
+        if (opt[key] != null) res[key] = opt[key];
+        return res;
+      }, {} as IPluginOptions);
+
+      let archive;
+      if (opt.password) {
+        archiver.registerFormat('zip-encryptable', ZipEncryptable);
+        /** @ts-ignore */
+        archive = archiver('zip-encryptable', opt);
+      } else {
+        archive = archiver('zip', opt);
+      }
+
+      const writeStream = fs.createWriteStream(outFilePath);
+      archive.pipe(writeStream);
+      fileList.forEach(({ path, fileName }) => {
+        archive.file(path, { name: fileName });
+      });
+
+      archive.finalize();
+      zipCreated = true;
+      console.log(green('created zip file:') + ' ' + green(`${bold(outFilePath)}`));
+    }
   };
 };
